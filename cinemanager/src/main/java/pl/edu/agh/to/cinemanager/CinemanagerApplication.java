c9e77b6c836cf8db17bcd1f985d10522a354ed02
Krzysztof Ligarski <ligarski.k@gmail.com>
2024-12-01 12:54:30 +0100

    Initial SpringBoot setup, .gitignore

---
+++ cinemanager/src/main/java/pl/edu/agh/to/cinemanager/CinemanagerApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.to.cinemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemanagerApplication.class, args);
+	}
+
+}
