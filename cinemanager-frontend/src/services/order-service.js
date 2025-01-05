d815e669041e867e1df4cf73cf307464de9d2c57
Tomasz <tom.pl@op.pl>
2025-01-05 16:30:04 +0100

    Added fully functional seat and ticket type selection with order placing method

--- cinemanager-frontend/src/services/order-service.js
+++ cinemanager-frontend/src/services/order-service.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { API_URL_BASE } from "./api-url-config";
+import { getCurrentUser } from "./authentication-service";
 
 const API_URL = API_URL_BASE
 
@@ -9,4 +10,25 @@ export async function fetch_screening_info(screeningId){
 
 export async function fetch_screening_seats(screeningId){
     return axios.get(API_URL + "screenings/" + screeningId + "/seats" , {}, {}).catch(err => console.warn(err))
+}
+
+export function place_order(tickets, screeningId){
+    const ticketsDto = []
+    for(var ticket of tickets){
+        ticketsDto.push(
+            {
+                "row": ticket.row,
+                "seatNumber": ticket.position,
+                "ticketType": ticket.type=="Ulgowy"?"DISCOUNTED":"REGULAR"
+            }
+        )
+    }
+    return axios.post(API_URL + "orders", {
+        "screeningId": screeningId,
+        "tickets": ticketsDto
+    }, {
+        headers: {
+            Authorization: "Bearer " + getCurrentUser()
+        }
+    }).catch(err => console.warn(err))
 }
