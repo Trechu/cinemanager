81f1a6a15dd859fe6dfd72038b9e8120ef9296cd
Krzysztof Ligarski <ligarski.k@gmail.com>
2025-01-11 11:23:29 +0100

    GET by id, PUT, DELETE endpoints for reviews

--- cinemanager/src/main/java/pl/edu/agh/to/cinemanager/controller/ReviewController.java
+++ cinemanager/src/main/java/pl/edu/agh/to/cinemanager/controller/ReviewController.java
@@ -4,14 +4,21 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import pl.edu.agh.to.cinemanager.dto.RequestReviewDto;
 import pl.edu.agh.to.cinemanager.dto.ResponseReviewDto;
 import pl.edu.agh.to.cinemanager.model.Movie;
 import pl.edu.agh.to.cinemanager.model.Review;
 import pl.edu.agh.to.cinemanager.model.User;
 import pl.edu.agh.to.cinemanager.repository.specification.ReviewSpecification;
 import pl.edu.agh.to.cinemanager.service.ReviewService;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(path = "api/reviews")
@@ -28,5 +35,37 @@ public class ReviewController {
                 .and(ReviewSpecification.user(user));
 
         return reviewService.getAllReviews(reviewSpecification, pageable);
+    }
+
+    @GetMapping("{id}")
+    public ResponseReviewDto getReviewById(@PathVariable("id") Review review) {
+        return reviewService.reviewToResponseDto(review);
+    }
+
+    @PostMapping("")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<ResponseReviewDto> createReview(Authentication authentication, @RequestBody RequestReviewDto reviewDto) {
+        ResponseReviewDto responseReviewDto = reviewService.createReviewByEmail(authentication.getName(), reviewDto);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .replacePath("/api/reviews/{id}")
+                .buildAndExpand(responseReviewDto.id()).toUri();
+
+        return ResponseEntity.created(location).body(responseReviewDto);
+    }
+
+    @PutMapping("{id}")
+    @PreAuthorize("isAuthenticated()")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateReview(@PathVariable("id") Review review, @RequestBody RequestReviewDto reviewDto, Authentication authentication) {
+        reviewService.updateReview(authentication, review, reviewDto);
+    }
+
+    @DeleteMapping("{id}")
+    @PreAuthorize("isAuthenticated()")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteReview(@PathVariable("id") Review review, Authentication authentication) {
+        reviewService.deleteReview(authentication, review);
     }
 }
--- cinemanager/src/main/java/pl/edu/agh/to/cinemanager/service/ReviewService.java
+++ cinemanager/src/main/java/pl/edu/agh/to/cinemanager/service/ReviewService.java
@@ -1,23 +1,35 @@
 package pl.edu.agh.to.cinemanager.service;
 
+import jakarta.validation.ConstraintViolationException;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import pl.edu.agh.to.cinemanager.dto.RequestReviewDto;
 import pl.edu.agh.to.cinemanager.dto.ResponseReviewDto;
 import pl.edu.agh.to.cinemanager.dto.ResponseReviewUserDto;
+import pl.edu.agh.to.cinemanager.model.Movie;
 import pl.edu.agh.to.cinemanager.model.Review;
 import pl.edu.agh.to.cinemanager.model.User;
+import pl.edu.agh.to.cinemanager.model.UserRole;
 import pl.edu.agh.to.cinemanager.repository.ReviewRepository;
+
+import java.math.BigDecimal;
 
 @Service
 @AllArgsConstructor
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
+    private final UserService userService;
+    private final MovieService movieService;
+    private final AuthService authService;
 
     public Page<ResponseReviewDto> getAllReviews(Specification<Review> specification, Pageable pageable) {
         return reviewRepository
@@ -26,6 +38,84 @@ public class ReviewService {
                     pageable.getPageSize(),
                     pageable.getSortOr(Sort.by(Sort.Direction.DESC, "id"))))
                 .map(this::reviewToResponseDto);
+    }
+
+    public ResponseReviewDto createReviewByEmail(String email, RequestReviewDto requestReviewDto) {
+        User user = userService.findUserByEmail(email).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "This user does not exist")
+        );
+
+        Movie movie = movieService.getMovieById(requestReviewDto.movieId()).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "This movie does not exist")
+        );
+
+        Review review = new Review(requestReviewDto.rating(), requestReviewDto.content(), user, movie);
+
+        validateAndSave(review);
+
+        return reviewToResponseDto(review);
+    }
+
+    public void updateReview(Authentication authentication, Review review, RequestReviewDto updatedDto) {
+        User requestUser = userService.findUserByEmail(authentication.getName()).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "This user does not exist")
+        );
+
+        if (review.getUser().getEmail().equals(requestUser.getEmail())
+                || authService.hasRole(UserRole.MANAGER, authentication.getAuthorities())) {
+            Movie movie = movieService.getMovieById(updatedDto.movieId()).orElseThrow(
+                    () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "This movie does not exist")
+            );
+
+            review.setContent(updatedDto.content());
+            review.setRating(updatedDto.rating());
+            review.setMovie(movie);
+
+            validateAndSave(review);
+        } else {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
+    }
+
+    public void deleteReview(Authentication authentication, Review review) {
+        User requestUser = userService.findUserByEmail(authentication.getName()).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "This user does not exist")
+        );
+
+        if (review.getUser().getEmail().equals(requestUser.getEmail())
+                || authService.hasRole(UserRole.MANAGER, authentication.getAuthorities())) {
+            reviewRepository.delete(review);
+        } else {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
+    }
+
+    private void validateAndSave(Review review) {
+        try {
+            if (!isValidRating(review.getRating())) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                        "Rating must be a number from 0.0 to 5.0 with a step of 0.5");
+            }
+            reviewRepository.save(review);
+        } catch (ResponseStatusException e) {
+            throw e;
+        } catch (Exception e) {
+            if (e.getCause().getCause() instanceof ConstraintViolationException) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+            } else {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+            }
+        }
+    }
+
+    private boolean isValidRating(BigDecimal rating) {
+        BigDecimal minRating = BigDecimal.valueOf(0.0);
+        BigDecimal maxRating = BigDecimal.valueOf(5.0);
+        BigDecimal stepRating = BigDecimal.valueOf(0.5);
+
+        return rating.compareTo(minRating) >= 0
+                && rating.compareTo(maxRating) <= 0
+                && rating.remainder(stepRating).compareTo(BigDecimal.ZERO) == 0;
     }
 
     public ResponseReviewDto reviewToResponseDto(Review review) {
